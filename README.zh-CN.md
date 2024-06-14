d216c8ade61938e960c2c20cb803e2b4b601dbfc
testnet0 <wangxinwei01@kanzhun.com>
2024-06-14 19:31:54 +0800

    first commit

---
+++ README.zh-CN.md
@@ -0,0 +1,55 @@
+## TestNet 资产管理系统介绍
+<div align="center">
+
+简体中文 / [English](./README.md)
+
+</div>
+TestNet资产管理系统旨在提供全面、高效的互联网资产管理与监控服务，构建详细的资产信息库。该系统能够帮助企业安全团队或渗透测试人员对目标资产进行深入侦察和分析，提供攻击者视角的持续风险监测，协助用户实时掌握资产动态，识别并修复安全漏洞，从而有效收敛攻击面，提升整体安全防护能力。
+
+### 目前功能
+
+- [X] 项目管理
+- [X] 资产管理（公司、域名、子域名、IP、端口、Web、API、漏洞、资产标签、黑名单等）
+- [X] 资产导入导出
+- [X] 高级搜索
+- [X] 扫描脚本定制
+- [X] 批量扫描 & 定时任务
+- [X] 节点配置自定义
+
+**自带工具的脚本，也可以根据需要加入其他工具：**
+
+- [X] 子域名扫描（OneForAll、subfinder）
+- [X] 端口扫描（Nmap、Naabu）
+- [X] Web探测（Httpx）
+- [X] 漏洞扫描（Nuclei）
+- [X] DNS解析（CDN及存活判断）
+- [X] 敏感目录扫描（DirSearch）
+- [X] ICP备案查询
+- [X] 0.zone API调用
+
+#### 快速开始
+
+### 1、 **快速安装**
+### Linux和Mac
+
+```bash
+git clone https://github.com/testnet0/testnet_build.git
+cd testnet_build && bash build.sh
+```
+稍等片刻，即可启动系统。默认访问端口为 `IP:80`
+### Windows
+ 参考帮助文档
+
+### 2、 **默认密码**
+   - **安全测试**：`TestNet/TestNet123@`
+   - **管理员**：`admin/123456`
+
+### 3、系统界面
+![](https://raw.githubusercontent.com/testnet0/testnet/main/doc/img/dashboard.png)
+
+### 4、联系我们
+- 微信群：
+  ![](https://raw.githubusercontent.com/testnet0/testnet/main/doc/img/wechat.png)
+### 详细文档链接
+
+- [TestNet 资产管理系统帮助文档](https://www.yuque.com/testnet-niqki/etr5ls)
---
+++ build.sh
@@ -0,0 +1,132 @@
+#!/bin/bash
+
+echo "
+████████╗███████╗███████╗████████╗███╗   ██╗███████╗████████╗
+╚══██╔══╝██╔════╝██╔════╝╚══██╔══╝████╗  ██║██╔════╝╚══██╔══╝
+   ██║   █████╗  ███████╗   ██║   ██╔██╗ ██║█████╗     ██║
+   ██║   ██╔══╝  ╚════██║   ██║   ██║╚██╗██║██╔══╝     ██║
+   ██║   ███████╗███████║   ██║   ██║ ╚████║███████╗   ██║
+   ╚═╝   ╚══════╝╚══════╝   ╚═╝   ╚═╝  ╚═══╝╚══════╝   ╚═╝
+"
+
+# 检查命令
+function command_exists() {
+    command -v "$1" >/dev/null 2>&1
+}
+
+confirm() {
+    echo -e -n "\033[34m[TestNet] $* \033[1;36m(Y/n)\033[0m"
+    read -n 1 -s opt
+
+    [[ "$opt" == $'\n' ]] || echo
+
+    case "$opt" in
+        'y' | 'Y' ) return 0;;
+        'n' | 'N' ) return 1;;
+        *) confirm "$1";;
+    esac
+}
+
+info() {
+    echo -e "\033[37m[TestNet] $*\033[0m"
+}
+
+warning() {
+    echo -e "\033[33m[TestNet] $*\033[0m"
+}
+
+abort() {
+    echo -e "\033[31m[TestNet] $*\033[0m"
+    exit 1
+}
+
+trap 'onexit' INT
+onexit() {
+    echo
+    abort "用户手动结束安装"
+}
+
+testnet_path='/data/testnet'
+
+if [ -z "$BASH" ]; then
+    abort "请用 bash 执行本脚本"
+fi
+
+if [ ! -t 0 ]; then
+    abort "STDIN 不是标准的输入设备"
+fi
+
+if [ "$#" -ne "0" ]; then
+    abort "当前脚本无需任何参数"
+fi
+
+info "脚本调用方式确认正常"
+
+if ! command_exists docker; then
+    warning "缺少 Docker 环境"
+    if confirm "是否需要自动安装 Docker"; then
+        if ! curl -sSLk https://get.docker.com/ | bash -s -- --mirror; then
+            abort "Docker 安装失败，请检查网络连接或尝试手动安装"
+        fi
+        info "Docker 安装完成"
+    else
+        abort "中止安装"
+    fi
+fi
+
+info "发现 Docker 环境: '$(command -v docker)'"
+
+docker version > /dev/null 2>&1
+if [ $? -ne "0" ]; then
+    abort "Docker 服务工作异常"
+fi
+info "Docker 工作状态正常"
+
+compose_command="docker compose"
+if $compose_command version; then
+    info "发现 Docker Compose Plugin"
+else
+    warning "未发现 Docker Compose Plugin"
+    compose_command="docker-compose"
+    if [ -z `command_exists "docker-compose"` ]; then
+        warning "未发现 docker-compose 组件"
+        if confirm "是否需要自动安装 Docker Compose Plugin"; then
+            curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+            if [ $? -ne "0" ]; then
+                abort "Docker Compose Plugin 安装失败"
+            fi
+            info "Docker Compose Plugin 安装完成"
+            compose_command="docker compose"
+        else
+            abort "中止安装"
+        fi
+    else
+        info "发现 docker-compose 组件: '`command -v docker-compose`'"
+    fi
+fi
+
+# 检查是否已经存在 .env 文件
+if [ -f ".env" ]; then
+    echo "文件已存在"
+else
+    # 如果不存在，则创建 .env 文件
+    touch ".env"
+    if [ $? -ne "0" ]; then
+        echo "创建 .env 文件失败"
+    else
+        echo "创建 .env 文件成功"
+        echo "REDIS_PASSWORD=$(LC_ALL=C tr -dc A-Za-z0-9 </dev/urandom | head -c 32)" >> .env
+        echo "MYSQL_PASSWORD=$(LC_ALL=C tr -dc A-Za-z0-9 </dev/urandom | head -c 32)" >> .env
+        echo "SUBNET_PREFIX=172.16.1" >> .env
+    fi
+fi
+
+$compose_command up -d
+
+
+
+if confirm "是否需要自动安装运行环境"; then
+  docker exec testnet-client /bin/bash -c "cd /testnet-client && chmod +x ./start.sh && ./start.sh"
+else
+ abort "取消安装运行环境"
+fi
