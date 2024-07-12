bffb05fdacd9f795f93f7d0ea1ad6c67055a9b4f
testnet0 <wangxinwei01@kanzhun.com>
2024-07-12 18:11:01 +0800

    更新安装脚本

--- build.sh
+++ build.sh
@@ -9,8 +9,8 @@ echo "
    ╚═╝   ╚══════╝╚══════╝   ╚═╝   ╚═╝  ╚═══╝╚══════╝   ╚═╝
 "
 
-# 检查命令
-function command_exists() {
+# Check if command exists
+command_exists() {
     command -v "$1" >/dev/null 2>&1
 }
 
@@ -83,15 +83,16 @@ fi
 info "Docker 工作状态正常"
 
 compose_command="docker compose"
-if $compose_command version; then
+if $compose_command version > /dev/null 2>&1; then
     info "发现 Docker Compose Plugin"
 else
     warning "未发现 Docker Compose Plugin"
     compose_command="docker-compose"
-    if [ -z `command_exists "docker-compose"` ]; then
+    if ! command_exists "docker-compose"; then
         warning "未发现 docker-compose 组件"
         if confirm "是否需要自动安装 Docker Compose Plugin"; then
             curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+            chmod +x /usr/local/bin/docker-compose
             if [ $? -ne 0 ]; then
                 abort "Docker Compose Plugin 安装失败"
             fi
@@ -101,19 +102,17 @@ else
             abort "中止安装"
         fi
     else
-        info "发现 docker-compose 组件: '`command -v docker-compose`'"
+        info "发现 docker-compose 组件: '$(command -v docker-compose)'"
     fi
 fi
 
-function create_env_file() {
-    # 检查是否已经存在 .env 文件
+create_env_file() {
     if [ -f ".env" ]; then
-        echo ".env 文件已存在"
+        info ".env 文件已存在"
     else
-        # 如果不存在，则创建 .env 文件
         touch ".env"
         if [ $? -ne 0 ]; then
-            echo "创建 .env 文件失败"
+            warning "创建 .env 文件失败"
         else
             echo "创建 .env 文件成功"
             echo "REDIS_PASSWORD=$(LC_ALL=C tr -dc A-Za-z0-9 </dev/urandom | head -c 32)" >> .env
@@ -123,48 +122,85 @@ function create_env_file() {
     fi
 }
 
-function create_es_data_folder() {
-    # 检查ES文件夹
+create_es_data_folder() {
     if [ -d "./es_data" ]; then
-        echo "文件夹已存在"
+        info "文件夹已存在"
     else
-        # 如果不存在，则创建 es_data 文件夹
         mkdir "./es_data"
         if [ $? -ne 0 ]; then
-            echo "创建 ./es_data 文件夹失败"
+            warning "创建 ./es_data 文件夹失败"
         else
             chmod 777 ./es_data
             if [ $? -ne 0 ]; then
-                echo "设置 ./es_data 文件夹权限失败"
+                warning "设置 ./es_data 文件夹权限失败"
             else
-                echo "成功创建并设置 ./es_data 文件夹"
+                info "成功创建并设置 ./es_data 文件夹"
             fi
         fi
     fi
 }
 
-function start_testnet() {
+start_testnet() {
     $compose_command up -d
     warning "TestNet安装成功，请稍等2分钟打开后台登录..."
     warning "后台访问地址：https://IP:8099/"
 }
 
-function stop_testnet() {
+stop_testnet() {
     $compose_command stop
     info "TestNet 已停止运行"
 }
 
-function update_testnet() {
-    info "开始更新 TestNet..."
+update_testnet_server() {
+    info "开始更新 TestNet 服务端..."
     git pull
     $compose_command stop testnet-server testnet-frontend
     $compose_command rm -f testnet-server testnet-frontend
     $compose_command pull testnet-server testnet-frontend
     $compose_command up -d testnet-server testnet-frontend
-    info "TestNet 更新完成"
+    info "TestNet 服务端更新完成"
 }
 
-function install_run_environment() {
+update_testnet_client() {
+    info "开始更新 TestNet客户端..."
+    if confirm "是否需要更新客户端？已安装工具需要重新安装"; then
+        $compose_command stop testnet-client
+        $compose_command pull testnet-client
+        $compose_command up -d testnet-client
+    else
+        abort "取消安装运行环境"
+    fi
+    info "TestNet 客户端更新完成"
+}
+
+local_ips() {
+    if command_exists ip; then
+        ip addr show | grep -Eo 'inet ([0-9]*\.){3}[0-9]*' | awk '{print $2}'
+    else
+        ifconfig -a | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | awk '{print $2}'
+    fi
+}
+
+ips=$(local_ips)
+
+start_testnet_server() {
+    $compose_command -f docker-compose-server.yml up -d
+    warning "TestNet安装成功，请稍等2分钟打开后台登录..."
+    warning "后台访问地址：https://0.0.0.0:8099/"
+    for ip in $ips; do
+        warning https://$ip:8099/
+    done
+}
+
+start_testnet_client() {
+    if [ -f ".env" ]; then
+        $compose_command -f docker-compose-client.yml up -d
+    else
+        warning "请先复制服务端配置文件到客户端，增加一个IP=你的IP地址"
+    fi
+}
+
+install_run_environment() {
     if confirm "是否需要自动安装客户端运行环境"; then
         docker exec testnet-client /bin/bash -c "cd /testnet-client && chmod +x ./start.sh && ./start.sh"
     else
@@ -172,12 +208,13 @@ function install_run_environment() {
     fi
 }
 
-# 提供用户操作选择
 echo "请选择操作："
-echo "1) 安装 TestNet"
-echo "2) 更新 TestNet"
-echo "3) 停止运行 TestNet"
-echo "4) 安装客户端运行环境"
+echo "1) 一键安装 TestNet服务端 + 客户端 (testnet-server testnet-frontend testnet-client)"
+echo "2) 单独安装 TestNet服务端 (testnet-server testnet-frontend)"
+echo "3) 单独安装 TestNet客户端(testnet-client)"
+echo "4) 更新服务端 (testnet-server testnet-frontend)"
+echo "5) 更新客户端 (testnet-client)"
+echo "6) 安装客户端运行环境"
 read -p "输入数字选择操作: " user_choice
 
 case $user_choice in
@@ -188,12 +225,22 @@ case $user_choice in
         install_run_environment
         ;;
     2)
-        update_testnet
+        create_env_file
+        create_es_data_folder
+        start_testnet_server
         ;;
     3)
-        stop_testnet
+        start_testnet_client
+        install_run_environment
         ;;
     4)
+        update_testnet_server
+        ;;
+    5)
+        update_testnet_client
+        install_run_environment
+        ;;
+    6)
         install_run_environment
         ;;
     *)
